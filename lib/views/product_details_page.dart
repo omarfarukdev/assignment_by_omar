a717aec21bbc2f1a6b24194e94095b41cf7136f2
FarukOmar <omar.faruq1998v@gmail.com>
2023-02-12 19:03:40 +0600

    create search page and product details page and controller

---
+++ lib/views/product_details_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatelessWidget {
+  const ProductDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
